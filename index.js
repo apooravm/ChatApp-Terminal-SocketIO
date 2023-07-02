16aebc9d4c7bb930d87f9b203cc0631b927eb7da
Apoorav <medal.apoorav@gmail.com>
2023-07-02 14:21:21 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Client = require("./src/client/client");
 const localPort = process.env.PORT;
 const AuthKey = process.env.PASS;
-const c1 = new Client(AuthKey, `http://localhost:${localPort}`);
+const localhost = `http://localhost:${localPort}`;
+const c1 = new Client(AuthKey, 'https://chat-app-terminal-socket-io.vercel.app/');
 c1.run();
